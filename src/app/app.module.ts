1fdd5135c8f4bbbb07926dcaface5372112dff67
Logan <loganshalom@gmail.com>
2023-12-01 20:02:50 -0300

    working carrousel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntradasComponent } from './homepage/entradas/entradas.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MenuComponent } from './homepage/menu/menu.component';
-import { EntradasComponent } from './homepage/entradas/entradas.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, MenuComponent, EntradasComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    MenuComponent,
+    EntradasComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    SlickCarouselModule,
   ],
   providers: [],
   bootstrap: [HomepageComponent],
--- src/app/homepage/entradas/entradas.component.ts
+++ src/app/homepage/entradas/entradas.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SlickCarouselComponent } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-entradas',
   templateUrl: './entradas.component.html',
-  styleUrls: ['./entradas.component.css']
+  styleUrls: ['./entradas.component.css'],
 })
 export class EntradasComponent {
+  @ViewChild('slickModal') slickModal!: SlickCarouselComponent;
 
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+    ],
+  };
+
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
 }
