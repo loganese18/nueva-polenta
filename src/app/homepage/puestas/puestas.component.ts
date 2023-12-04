70cedd4f1b36c7466318d0bf758eb1191ff2807b
Logan <loganshalom@gmail.com>
2023-12-04 09:18:15 -0300

    working puestas

--- src/app/homepage/puestas/puestas.component.ts
+++ src/app/homepage/puestas/puestas.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-puestas',
   templateUrl: './puestas.component.html',
-  styleUrls: ['./puestas.component.css']
+  styleUrls: ['./puestas.component.css'],
 })
-export class PuestasComponent {
+export class PuestasComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      autoplay: false,
+      centeredSlides: true,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+      lazyPreloadPrevNext: 2,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Permite hacer clic en los puntos para navegar a la diapositiva correspondiente
+      },
+      modules: [Navigation, Pagination],
+    });
+    console.log('Inicialización de Swiper');
+  }
 
+  puestas_mobile: string[] = [
+    './assets/puestas-mobile/mobile-puesta1.png',
+    './assets/puestas-mobile/mobile-puesta2.png',
+    './assets/puestas-mobile/mobile-puesta3.png',
+    './assets/puestas-mobile/mobile-puesta4.png',
+    './assets/puestas-mobile/mobile-puesta5.png',
+    './assets/puestas-mobile/mobile-puesta6.png',
+    './assets/puestas-mobile/mobile-puesta7.png',
+    './assets/puestas-mobile/mobile-puesta8.png',
+    './assets/puestas-mobile/mobile-puesta9.png',
+    './assets/puestas-mobile/mobile-puesta10.png',
+    './assets/puestas-mobile/mobile-puesta11.png',
+    './assets/puestas-mobile/mobile-puesta12.png',
+  ];
+  puestas_desktop: string[] = [
+    './assets/puestas-desktop/desktop-puesta1.png',
+    './assets/puestas-desktop/desktop-puesta2.png',
+    './assets/puestas-desktop/desktop-puesta3.png',
+    './assets/puestas-desktop/desktop-puesta4.png',
+    './assets/puestas-desktop/desktop-puesta5.png',
+    './assets/puestas-desktop/desktop-puesta6.png',
+    './assets/puestas-desktop/desktop-puesta7.png',
+    './assets/puestas-desktop/desktop-puesta8.png',
+    './assets/puestas-desktop/desktop-puesta9.png',
+  ];
+
+  isMobileScreen = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    if (window.innerWidth > 950) {
+      this.isMobileScreen = false;
+    } else {
+      this.isMobileScreen = true;
+    }
+  }
 }
